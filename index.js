9810b9e69863526d8478728f21fccf1c6553199f
Alvar Coll Mutikainen <alvarcoll310@gmail.com>
2025-04-07 18:42:17 +0200

    First message test

--- index.js
+++ index.js
@@ -3,5 +3,14 @@
 import bencode from "bencode";
 import fs from "fs";
 
+import dgram from "dgram";
+import { Buffer } from "buffer";
+
 const torrent = bencode.decode(fs.readFileSync("puppy.torrent"));
-console.log(torrent.announce.toString("utf8"));
+const url = new URL(torrent.announce.toString("utf-8"));
+const socket = dgram.createSocket("udp4");
+const myMsg = Buffer.from("hello?", "utf-8");
+socket.send(myMsg, 0, myMsg.length, url.port, url.hostname, () => {});
+socket.on("message", (msg) => {
+  console.log("message is", msg);
+});
